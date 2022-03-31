6b9f9975b82552428165c0a065d646c60a02e83f
Sabino Eduardo <bilson@outlook.com.br>
2022-03-30 22:36:53 -0300

    Update exe01.py

--- exercicios/estrutura_de_repetição/exe01.py
+++ exercicios/estrutura_de_repetição/exe01.py
@@ -1,11 +1,12 @@
 #
-#? Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+# ? Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue
+# pedindo até que o usuário informe um valor válido.
 
 while True:
     print("Digite uma nota entre 0 a 10.")
     nota = float(input("Nota:"))
-    if nota <= 10 and nota >= 0:
-        print("Correto")
+    if 0 <= nota <= 10:
+        print("Correct")
         break
     else:
-        print("Valor inválido")
+        print("Value invalid")
